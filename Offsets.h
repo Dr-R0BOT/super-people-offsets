dbe9799ea7d8968ad10f9e9b01903764630dfe13
Joe <93642070+Dr-R0BOT@users.noreply.github.com>
2021-12-16 04:03:20 -0800

    Create Offsets.h
    lol

---
+++ Offsets.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Includes.h"
+
+namespace offset
+{
+	    const DWORD Uworld = 0x6958720;
+		const DWORD Gnames = 0x67F7400;
+		const DWORD UObjects = 0x680FCC0;
+		const DWORD BoneMatrix = 0x36FF400;
+		const DWORD WorldToScreen = 0x37E2460;
+		const DWORD LineOfSightTo = 0x372FA50;
+		const DWORD LocalPlayers = 0xb8;
+		const DWORD GameInstance = 0x1d8;
+		const DWORD PersistentLevel = 0x48;
+		const DWORD Levels = 0x198;
+		const DWORD PlayerController = 0x30;
+		const DWORD PlayerCameraManager = 0x438;
+		const DWORD AcknowledgedPawn = 0x428;
+		const DWORD PlayerState = 0x340;
+		const DWORD TeamId = 0x598;
+		const DWORD RootComponent = 0x158;
+		const DWORD PlayerState = 0x340;
+		const DWORD RootComponent = 0x158;
+		const DWORD Mesh = 0x398;
+		const DWORD RelativeLocation = 0x224;
+		const DWORD PlayerName = 0x3f8;
+		const DWORD Health = 0x10e0;
+		const DWORD CameraCache = 0x380;
+		const DWORD IsBot = 0x322;
+		const DWORD MaxWalkSpeed = 0x300;
+		const DWORD MaxSwimSpeed = 548;
+		const DWORD MaxFlySpeed = 0x4e8;
+		const DWORD MaxCustomMovmentSpeed = 0x460;
+}
